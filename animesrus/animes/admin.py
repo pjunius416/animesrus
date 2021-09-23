360df9be135bad7076222826f206270e79ae8273
pjunius416 <52839119+pjunius416@users.noreply.github.com>
2021-09-23 02:50:51 -0500

    Added Animes and Reviews to admin panel

--- animesrus/animes/admin.py
+++ animesrus/animes/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Anime, Review
+
+class ReviewAdmin(admin.ModelAdmin):
+    model = Review
+    list_display = ( 'review', 'anime_name', 'author' )
+
+class ReviewInLine(admin.TabularInline):
+    model = Review
+    
+class AnimeAdmin(admin.ModelAdmin):
+    list_display = ( 'anime_name', 'language_type', 'on_hulu', 'on_netflix', 'on_crunchyroll', 'on_funamation', 'added_by' )
+    inlines = [
+        ReviewInLine,
+    ]
+
+admin.site.register(Anime, AnimeAdmin)
+admin.site.register(Review, ReviewAdmin)
--- animesrus/animes/models.py
+++ animesrus/animes/models.py
@@ -1,3 +1,40 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Anime(models.Model):
+    anime_name = models.CharField(max_length=100, blank=False, null=False, default='')
+    description = models.TextField()
+    language_type = models.CharField(max_length=20, blank=False, null=False, default='')
+    on_hulu = models.BooleanField(default='')
+    on_netflix = models.BooleanField(default='')
+    on_crunchyroll = models.BooleanField(default='')
+    on_funamation = models.BooleanField(default='')
+    added_by = models.ForeignKey(
+        get_user_model(), 
+        on_delete=models.SET('User Deleted'), 
+    )
+    last_updated = models.DateTimeField(auto_now_add=True)
+    
+    def updated(self):
+        self.last_updated = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return str(self.anime_name)
+    
+class Review(models.Model):
+    anime_name = models.ForeignKey(
+        Anime,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+    review = models.TextField()
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.review or ''
