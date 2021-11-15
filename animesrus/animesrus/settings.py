5c58647d4ea22bb523b86e13ee21b7976aeb7fa0
pjunius416 <52839119+pjunius416@users.noreply.github.com>
2021-11-14 20:34:29 -0600

    Adding AWS Staticfile Method

--- animesrus/animesrus/settings.py
+++ animesrus/animesrus/settings.py
@@ -130,6 +130,18 @@ USE_L10N = True
 
 USE_TZ = True
 
+#AWS Settings
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
+
+AWS_DEFAULT_ACL = None
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
 # # Static files (CSS, JavaScript, Images)
 # # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
